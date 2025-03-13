2283f37b95d92e4ac2950ed179efd118206af633
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-13 11:39:57 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:e_commerce_app/views/auth/ui/login_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shoply',
+      theme: ThemeData(useMaterial3: true),
+      home: const LoginView(),
+    );
+  }
+}
