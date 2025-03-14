29323b9eb89e4b7a6bc0ad19287bd5547c9ee8fa
mahmoudmohamed17 <ma5975986@gmail.com>
2025-03-14 14:38:29 +0200

    finish favorite products

--- lib/views/favorites/ui/favorites_view_widget.dart
+++ lib/views/favorites/ui/favorites_view_widget.dart
@@ -1,3 +1,5 @@
+import 'package:e_commerce_app/core/utils/app_text_styles.dart';
+import 'package:e_commerce_app/views/home/ui/recently_products_list.dart';
 import 'package:flutter/material.dart';
 
 class FavoritesViewWidget extends StatelessWidget {
@@ -5,6 +7,24 @@ class FavoritesViewWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(child: SizedBox(height: 16)),
+            SliverToBoxAdapter(
+              child: Text(
+                'Your favorite products',
+                style: AppTextStyles.bold16,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 16)),
+            SliverToBoxAdapter(child: RecentlyProductsList()),
+          ],
+        ),
+      ),
+    );
   }
 }
