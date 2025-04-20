c07f6b2286a39f1679511dbae597d1bafce6d22e
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-20 15:18:09 +0200

    customize snack bar

--- lib/core/helpers/snack_bar.dart
+++ lib/core/helpers/snack_bar.dart
@@ -4,15 +4,15 @@ import 'package:flutter/material.dart';
 void snackBar(BuildContext context, String message) {
   Flushbar(
     message: message,
-    icon: const Icon(Icons.notifications, color: Colors.white),
-    duration: const Duration(seconds: 3),
+    icon: const Icon(Icons.error, color: Colors.white),
+    duration: const Duration(seconds: 5),
     flushbarPosition: FlushbarPosition.TOP,
     backgroundColor: Colors.black,
     shouldIconPulse: false,
     margin: const EdgeInsets.all(16),
     borderRadius: BorderRadius.circular(100),
-    animationDuration: const Duration(milliseconds: 300),
-    forwardAnimationCurve: Curves.easeInOut,
+    animationDuration: const Duration(milliseconds: 500),
+    forwardAnimationCurve: Curves.easeIn,
     reverseAnimationCurve: Curves.easeOut,
     blockBackgroundInteraction: true,
   ).show(context);
