510e8c92c18f0e51509cef855e632dcc2d5aeb58
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-20 14:31:38 +0200

    create login and signup methods

--- lib/views/auth/logic/auth_cubit/auth_cubit.dart
+++ lib/views/auth/logic/auth_cubit/auth_cubit.dart
@@ -1,6 +1,37 @@
+import 'dart:developer';
+
 import 'package:bloc/bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   AuthCubit() : super(AuthInitial());
+
+  var client = Supabase.instance.client;
+
+  Future<void> login(String email, String password) async {
+    emit(LoginLoading());
+    try {
+      await client.auth.signInWithPassword(password: password, email: email);
+      emit(LoginSuccess());
+    } on AuthException catch (e) {
+      log('Error with AuthException: ${e.toString()}');
+      emit(LoginFailure(message: e.message));
+    } catch (e) {
+      emit(LoginFailure(message: e.toString()));
+    }
+  }
+
+  Future<void> signup(String email, String password) async {
+    emit(SignupLoading());
+    try {
+      await client.auth.signUp(password: password, email: email);
+      emit(SignupSuccess());
+    } on AuthException catch (e) {
+      log('Error with AuthException: ${e.toString()}');
+      emit(SignupFailure(message: e.message));
+    } catch (e) {
+      emit(SignupFailure(message: e.toString()));
+    }
+  }
 }
--- lib/views/auth/logic/auth_cubit/auth_state.dart
+++ lib/views/auth/logic/auth_cubit/auth_state.dart
@@ -8,10 +8,18 @@ final class LoginLoading extends AuthState {}
 
 final class LoginSuccess extends AuthState {}
 
-final class LoginFailure extends AuthState {}
+final class LoginFailure extends AuthState {
+  final String message;
+
+  LoginFailure({required this.message});
+}
 
 final class SignupLoading extends AuthState {}
 
 final class SignupSuccess extends AuthState {}
 
-final class SignupFailure extends AuthState {}
+final class SignupFailure extends AuthState {
+  final String message;
+
+  SignupFailure({required this.message});
+}
