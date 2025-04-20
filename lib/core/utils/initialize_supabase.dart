a52e5148f44e22c615573e239da57b2339ac9741
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-20 10:23:47 +0200

    secure supabase project sensitive data

--- lib/core/utils/initialize_supabase.dart
+++ lib/core/utils/initialize_supabase.dart
@@ -1,8 +1,9 @@
+import 'package:e_commerce_app/core/utils/api_strings.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> initializeSupabase() async {
-   await Supabase.initialize(
-    url: 'https://pnpejwgmvkzfmmtotlnf.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBucGVqd2dtdmt6Zm1tdG90bG5mIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUxMzU3MjEsImV4cCI6MjA2MDcxMTcyMX0.Gizb68SNqMnGG1YZYtWaVxGDeergIpO9fyyeW-dwqZ4',
+  await Supabase.initialize(
+    url: ApiStrings.myProjectUrl,
+    anonKey: ApiStrings.myAPiKey,
   );
 }
