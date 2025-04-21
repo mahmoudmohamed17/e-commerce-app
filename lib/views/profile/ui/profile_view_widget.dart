884cbdb2733eed7264b56a2b1cbb07c44c18efc2
mahmoudmohamed17 <ma5975986@gmail.com>
2025-04-21 13:54:55 +0200

    provide auth cubit to profile view

--- lib/views/profile/ui/profile_view_widget.dart
+++ lib/views/profile/ui/profile_view_widget.dart
@@ -1,11 +1,15 @@
 import 'package:e_commerce_app/core/helpers/app_colors.dart';
+import 'package:e_commerce_app/core/helpers/custom_loading_indicator.dart';
 import 'package:e_commerce_app/core/helpers/custom_profile_button.dart';
 import 'package:e_commerce_app/core/helpers/navigation_extension.dart';
+import 'package:e_commerce_app/core/helpers/snack_bar.dart';
 import 'package:e_commerce_app/core/utils/app_text_styles.dart';
+import 'package:e_commerce_app/views/auth/logic/cubit/auth_cubit.dart';
 import 'package:e_commerce_app/views/profile/logic/profile_button_model.dart';
 import 'package:e_commerce_app/views/profile/ui/edit_name_view.dart';
 import 'package:e_commerce_app/views/profile/ui/my_orders_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ProfileViewWidget extends StatelessWidget {
@@ -29,50 +33,72 @@ class ProfileViewWidget extends StatelessWidget {
     ProfileButtonModel(
       label: 'Logout',
       icon: FontAwesomeIcons.rightFromBracket,
-      onPressed: (context) {},
+      onPressed: (context) {
+        context.read<AuthCubit>().signout();
+      },
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        color: Colors.white,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 24),
-              const CircleAvatar(
-                backgroundColor: AppColors.kPrimaryColor,
-                radius: 35,
-                child: Icon(FontAwesomeIcons.solidUser, color: Colors.white),
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is SignOutSuccess) {
+          snackBar(context, 'Logout Successfully!');
+        }
+        if (state is SignOutFailure) {
+          snackBar(context, state.message);
+        }
+      },
+      builder: (context, state) {
+        return CustomLoadingIndicator(
+          inAsyncCall: state is SignOutLoading,
+          child: Center(
+            child: Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
               ),
-              const SizedBox(height: 8),
-              const Text('Mahmoud Mohamed', style: AppTextStyles.bold12),
-              const SizedBox(height: 8),
-              const Text(
-                'Flutter Developer . mahmoud@gmail.com',
-                style: AppTextStyles.medium12,
+              margin: const EdgeInsets.symmetric(horizontal: 16),
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 24),
+                    const CircleAvatar(
+                      backgroundColor: AppColors.kPrimaryColor,
+                      radius: 35,
+                      child: Icon(
+                        FontAwesomeIcons.solidUser,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text('Mahmoud Mohamed', style: AppTextStyles.bold12),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'Flutter Developer . mahmoud@gmail.com',
+                      style: AppTextStyles.medium12,
+                    ),
+                    const SizedBox(height: 24),
+                    Column(
+                      children: List.generate(_buttons.length, (index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: CustomProfileButton(model: _buttons[index]),
+                        );
+                      }),
+                    ),
+                    const SizedBox(height: 24),
+                  ],
+                ),
               ),
-              const SizedBox(height: 24),
-              Column(
-                children: List.generate(_buttons.length, (index) {
-                  return Padding(
-                    padding: const EdgeInsets.only(bottom: 16),
-                    child: CustomProfileButton(model: _buttons[index]),
-                  );
-                }),
-              ),
-              const SizedBox(height: 24),
-            ],
+            ),
           ),
-        ),
-      ),
+        );
+      },
     );
   }
 }
